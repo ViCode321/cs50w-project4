4aaff3656d3562ab50f70e2aa366e4b1928b86f6
ViCode321 <110791992+ViCode321@users.noreply.github.com>
2023-11-22 16:58:20 -0600

    ..

--- social_web/settings.py
+++ social_web/settings.py
@@ -99,23 +99,17 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
-{
+    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
         'OPTIONS': {
             'min_length': 4,
         }
-    },    
-    """{
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },"""        
+    },        
 ]
 
+#{'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},{
+#'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},{
+# 'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
