7dfc88ba4c479ec9456df3e2edc7235b2c50d7a2
ViCode321 <110791992+ViCode321@users.noreply.github.com>
2023-11-22 12:33:56 -0600

    ..

--- social_web/settings.py
+++ social_web/settings.py
@@ -11,12 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
-from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Cargar variables de entorno desde el archivo .env
+config.setup()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -37,7 +39,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-
 
 
 # Application definition
@@ -122,13 +123,21 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'  # Establece el idioma por defecto en español
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
+
+# Configuración de idiomas permitidos
+LANGUAGES = [
+    ('es', _('Spanish')),  # Añade 'es' a la lista de idiomas permitidos
+]
 
 
 # Static files (CSS, JavaScript, Images)
