5c3eb1a1f1b988d0db5cd0275c3d667eecfb0837
ViCode321 <110791992+ViCode321@users.noreply.github.com>
2023-11-22 18:12:10 -0600

    ..

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class Users(AbstractUser):
     UserID = models.AutoField(primary_key=True)
     Name = models.CharField(max_length=50)
     Last_name = models.CharField(max_length=50)  
-    Username = models.CharField(max_length=255, unique=True, default="default_username")       
+    Username = models.CharField(max_length=255, unique=True)       
     Email = models.EmailField(unique=True)
     Gener = models.CharField(max_length=255)
     PerfilPhoto = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
